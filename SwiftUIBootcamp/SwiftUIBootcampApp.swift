2b7c10a4e88be1e0f81eba5dc09bfcb12ddd7ad0
sagar <sagar@THINHYDL027.local>
2024-12-10 21:39:22 +0530

    initial project setup

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by sagar on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
